e1479f9d8645fed99868e12fc9620c5c46b05158
Aalok-Singh <singhaalok.6@gmail.com>
2023-11-26 22:12:10 +0530

    social layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+import CreatePost from './components/CreatePost'
+import Post from './components/Post'
 function App() {
  
   return (
-    <>
-  <div>
-    Placeholder
-  </div>
-    </>
+      <div className='app-container'>
+        <Sidebar/>
+        <div className='content'>
+        <Header/>
+        <CreatePost/>
+        <Post/>
+        <Footer/>
+        </div>
+      </div>
   )
 }
 
